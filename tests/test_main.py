b5c9ad18c78aac6b1b507fd0fbd13fd7f545c4ad
AleksMih <157414836+AleksMih@users.noreply.github.com>
2024-01-24 10:21:24 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+import pytest
 
 app = FastAPI()
 
 @app.get("/sum")
-async def get_sum(a: int, b: int):
+async def test_get_sum(a: int, b: int):
     result = a + b
     return {"sum": result}
